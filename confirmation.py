3fea4121d704207d4507d1e1a37490c0a8527cea
Jaeun8 <henryjeong8205@gmail.com>
2025-06-05 22:25:47 +0900

    feat: Add confirmation page to the navigation in platform_app.py

---
+++ confirmation.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.title("작성된 민원 확인하기")
--- platform_app.py
+++ platform_app.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 homepage = st.Page("homepage.py", title="홈페이지", icon=":material/home:")
 reporting_page = st.Page("reporting.py", title="민원 신청하기", icon=":material/add_location:")
+confirmation_page = st.Page("confirmation.py", title="작성된 민원 확인하기", icon=":material/check_circle:")
 
-pg = st.navigation([homepage, reporting_page])
+pg = st.navigation([homepage, reporting_page, confirmation_page])
 st.set_page_config(page_title="Homepage", page_icon=":material/home:")
 pg.run()
