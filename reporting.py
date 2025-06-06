b16464d34d7e65903f9070df19ca40a2292a109a
Jaeun8 <henryjeong8205@gmail.com>
2025-06-06 16:26:03 +0900

    refactor: Update title formatting and restructure map initialization in reporting.py

--- reporting.py
+++ reporting.py
@@ -3,22 +3,13 @@ import streamlit as st
 from folium.plugins import Draw
 from streamlit_folium import st_folium
 
-st.title('민원 등록하기')
+st.title('📝 민원 등록하기')
 
-m = folium.Map(location=[37.564375, 126.938871], zoom_start=16)
+def get_pos(lat, lng):
+    return lat, lng
 
-draw = Draw(
-    export=False,
-    draw_options={
-        "polyline": False,
-        "polygon": False,
-        "circle": False,
-        "rectangle": False,
-        "circlemarker": False,
-        "marker": True
-    },
-    edit_options={"edit": False}
-)
-draw.add_to(m)
+if "marker_location" not in st.session_state:
+    st.session_state.marker_location = [37.564375, 126.938871]
+    st.session_state.zoom = 16
 
-output = st_folium(m, width=725)
+m = folium.Map(location=st.session_state.marker_location, zoom_start=st.session_state.zoom)
