3e41eb36290683de3511bd742858237b7349916f
Jaeun8 <henryjeong8205@gmail.com>
2025-06-08 21:48:19 +0900

    fix: Update icon for confirmation page to improve clarity in navigation

--- platform_app.py
+++ platform_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 homepage = st.Page("homepage.py", title="홈페이지", icon=":material/home:")
 reporting_page = st.Page("reporting.py", title="민원 등록하기", icon=":material/add_location:")
-confirmation_page = st.Page("confirmation.py", title="등록된 민원 확인하기", icon=":material/check_circle:")
+confirmation_page = st.Page("confirmation.py", title="등록된 민원 확인하기", icon=":material/search:")
 
 pg = st.navigation([homepage, reporting_page, confirmation_page])
 
